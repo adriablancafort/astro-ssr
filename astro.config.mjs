dafb2aa693cf5a6a336bee7b31ed0b736e7191f3
Adria Blancafort <adriablancafort@gmail.com>
2024-03-17 12:09:54 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
